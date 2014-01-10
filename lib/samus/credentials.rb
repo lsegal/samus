506bf4de151d89e67ffdd1f68bc92330ee4c929b
Loren Segal <lsegal@soen.ca>
2014-01-09 18:56:51 -0800

    Add credentials resolver

---
+++ lib/samus/credentials.rb
@@ -0,0 +1,50 @@
+module Samus
+  class Credentials
+    attr_reader :name
+
+    @@credentials = {}
+
+    def initialize(name)
+      @name = name
+      load_credential_file
+    end
+
+    def load
+      return @@credentials[name] if @@credentials[name]
+
+      hsh = {}
+      data = nil
+      if File.executable?(@file)
+        data = `#{@file}`
+        if $?.to_i != 0
+          puts "[E] Loading credential #{name} failed with #{$?}"
+          exit(1)
+        end
+      else
+        data = File.read(@file)
+      end
+
+      data.split(/\r?\n/).each do |line|
+        name, value = *line.strip.split(':')
+        hsh["_creds_#{name.strip.downcase}"] = value.strip
+      end
+
+      @@credentials[name] = hsh
+    end
+
+    private
+
+    def load_credential_file
+      Samus.config_paths.each do |path|
+        file = File.join(path, 'credentials', name)
+        if File.exist?(file)
+          @file = file
+          return
+        end
+      end
+      puts "[E] Could not find credential: #{name} " +
+           "(SAMUS_CONFIG_PATH=#{Samus.config_paths.join(':')})"
+      exit(1)
+    end
+  end
+end
