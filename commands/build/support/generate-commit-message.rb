cc743361282869796e28ddd4d402c04537ecd530
Loren Segal <lsegal@soen.ca>
2014-03-22 14:45:44 -0700

    Add referenced issues since last tag to default generated commit message

---
+++ commands/build/support/generate-commit-message.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+def word_wrap(text)
+  text.gsub(/(.{1,60})(\s|$)/, "\\1\n  ")
+end
+
+def collect_issues
+  out = `git log $(git tag | tail -n 1)...HEAD -E --grep '#[0-9]+' 2>/dev/null`
+  issues = out.scan(/(#\d+)/).flatten
+end
+
+message = ENV["_message"]
+if message.nil? || message.strip.empty?
+  message = "Tag release v#{ENV["_version"]}"
+
+  issues = collect_issues
+  if issues.size > 0
+    message += "\n\nReferences:\n  " + word_wrap(issues.uniq.sort.join(", "))
+  end
+end
+
+puts message
